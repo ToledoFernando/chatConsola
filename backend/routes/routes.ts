4238c89138b2333e2620e0684defa1129aa2cc73
toledofernando <toledof764@gmail.com>
2023-07-04 01:51:23 -0300

    test

--- backend/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import users from "./users/users";
-
-const routes = Router();
-
-routes.use(users);
-
-export default routes;
--- src/types/users.ts
+++
@@ -1,5 +0,0 @@
-export interface Users {
-  username: string;
-  email: string;
-  password: string;
-}
